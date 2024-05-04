3d9b17c82a155431bb70754f66799e083ef812eb
kingsley achumie <kingsonly13c@gmail.com>
2024-05-04 16:00:07 +0100

    first commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+
+/**
+ * @OA\Info(
+ *     title="Notification Service ",
+ *     version="0.1"
+ * )
+ */
+class Controller extends BaseController
+{
+    use AuthorizesRequests, ValidatesRequests;
+}
