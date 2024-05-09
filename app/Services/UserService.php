253e7db1c608eb5ca370100ea00456da6d5d5674
Collins Nwoko C <collinsnwoko05@gmail.com>
2024-05-09 12:16:51 +0100

    wip:test, created user and user services jobs

---
+++ app/Services/UserService.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
+/**
+ * Class UserService
+ *
+ * Service class responsible for user-related operations.
+ *
+ * @package App\Services
+ */
+class UserService
+{
+
+    protected static ?string $password;
+    /**
+     * Get all users.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function get()
+    {
+        return User::all();
+    }
+
+    /**
+     * Show a user.
+     *
+     * @param int $id
+     * @return \App\Models\User|null
+     */
+    public function show(int $id): ?User
+    {
+        return User::findOrFail($id);
+    }
+
+    /**
+     * Create a new user.
+     *
+     * @param Request $request
+     * @return \App\Models\User
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function create(array $request): User
+    {
+        $request['password'] = static::$password ??= Hash::make('password');
+        $validatedData = $this->validateUserData($request);
+
+        $user = User::create($validatedData);
+
+        return $user;
+    }
+
+    /**
+     * Update an existing user.
+     *
+     * @param Request $request
+     * @param int $id
+     * @return \App\Models\User
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function update(array $request, int $id): User
+    {
+        $user = $this->show($id);
+        $validatedData = $this->validateUserData($request);
+
+        $user->fill($validatedData);
+        $user->save();
+
+        return $user;
+    }
+
+    /**
+     * Delete a user.
+     *
+     * @param int $id
+     * @return bool
+     * @throws \Exception
+     */
+    public function destroy(int $id): bool
+    {
+        $user = $this->show($id);
+        return $user->delete();
+    }
+
+    /**
+     * Validate user data.
+     *
+     * @param array $data
+     * @return array
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    private function validateUserData(array $data): array
+    {
+        $validator = Validator::make($data, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:users,email,' . ($data['id'] ?? null),
+        ]);
+
+        if ($validator->fails()) {
+            throw new ValidationException($validator);
+        }
+
+        return $validator->validated();
+    }
+}
