ac950f7d41ed969fae8bc2063d9e8dd74bdfbad8
Collins Nwoko C <collinsnwoko05@gmail.com>
2024-05-12 12:00:16 +0100

    all test passed

--- app/Jobs/CustomerCreated.php
+++ app/Jobs/CustomerCreated.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Services\CustomerService;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -13,16 +14,26 @@ class CustomerCreated implements ShouldQueue
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     private array $data;
+    private int $id;
 
 
     public function __construct(array $data)
     {
         $this->data = $data;
+        $this->id = $data['id'];
     }
 
 
     public function handle(): void
     {
+        $service = new CustomerService();
+        $customer = $service->show($this->id);
+
+        if (!$customer) {
+            $service->create($this->data);
+        } else {
+            $service->update($this->data, $this->id);
+        }
 
 
 
